859ced40ea3fb21406be30ba36b765bafe92ba45
miguel12344 <ronalmiguel2005@gmail.com>
2024-11-21 12:52:47 -0500

    fedd: mi app

--- mi_primera_app/lib/main.dart
+++ mi_primera_app/lib/main.dart
@@ -4,7 +4,9 @@ import 'package:provider/provider.dart';
 
 import 'my_home_page.dart';
 
-void main() {}
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
