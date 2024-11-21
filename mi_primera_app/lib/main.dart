0c815fbca61fa60acc43a3a5cc698d440f2edd04
miguel12344 <ronalmiguel2005@gmail.com>
2024-11-21 12:17:03 -0500

    fedd: mi app

---
+++ mi_primera_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:mi_primera_app/providers/my_app_state.dart';
+import 'package:provider/provider.dart';
+
+import 'my_home_page.dart';
+
+void main() {}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'My Primera App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
